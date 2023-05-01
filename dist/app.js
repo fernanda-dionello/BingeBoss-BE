7d66c320b4b8771b7cf7446b2f86362ed41607ca
fernanda-dionello <fernanda.dionello@gmail.com>
2023-05-01 18:54:40 -0300

    feat: add connection to mongodb

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db_connection = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@binge-boss-db.41afdlo.mongodb.net/?retryWrites=true&w=majority`;
+mongoose_1.default.connect(db_connection);
+const db = mongoose_1.default.connection;
+db.on("error", console.error.bind(console, "connection failed: "));
+db.once("open", function () {
+    console.log("Connected to the database successfully");
+});
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const db_connection = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@binge-boss-db.41afdlo.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(db_connection);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection failed: "));
+db.once("open", function () {
+  console.log("Connected to the database successfully");
+});
 const app = express();
 const port = process.env.PORT || 3000;
 
