39a177ea725b4842cc8ac5d4b4f7632c64bac71e
Fernanda Dionello <fernandadionello@Laptop-de-Fernanda.local>
2023-05-08 00:47:15 -0300

    feature: create logic to search content

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import ConnectDB from './config/index';
 import usersRoutes from './routes/usersRoutes';
+import searchRoutes from './routes/searchRoutes';
 
 const PORT = process.env.PORT || 3000;
 const API_URL = process.env.API_URL;
@@ -10,6 +11,7 @@ const API_URL = process.env.API_URL;
 const server = fastify();
 server.register(ConnectDB);
 server.register(usersRoutes);
+server.register(searchRoutes);
 
 server.listen({ port: +PORT}, (err, address = API_URL as string) => {
   if (err) {
---
+++ src/services/validators/searchValidators.ts
@@ -0,0 +1,24 @@
+import { searchQuery } from '../../controllers/searchController';
+import Joi from 'joi';
+import { errorHandler } from './common';
+
+export const SearchSchema = Joi.object({
+  title: Joi.string().required(),
+  adult: Joi.boolean().optional(),
+  language: Joi.string().optional(),
+  page: Joi.number().optional(),
+  type: Joi.string().valid("multi", "movie", "person","tv").required(),
+})
+const validateSearchQuery = (searchQuery: searchQuery)=> {
+  const result = SearchSchema.validate(searchQuery);
+  if (result.error) {
+    errorHandler(
+      "Missing",
+      result.error.details[0].message,
+      400,
+      "400"
+    )
+  }
+}
+
+export default validateSearchQuery;
